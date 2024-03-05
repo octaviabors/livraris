c5b22ed5e9466e2942ec9a1db4867ddee6a389d6
Darwin Sonia <162296916+octaviabors@users.noreply.github.com>
2024-03-05 10:56:25 +0200

    Create work.cpp

---
+++ work.cpp
@@ -0,0 +1,20 @@
+else if(formatChunk->audioFormat == WavAudioFormat::IeeeFloat) {
+        if(formatChunk->numChannels == 1 && formatChunk->bitsPerSample == 32)
+            _format = BufferFormat::MonoFloat;
+        else if(formatChunk->numChannels == 2 && formatChunk->bitsPerSample == 32)
+            _format = BufferFormat::StereoFloat;
+        else if(formatChunk->numChannels == 1 && formatChunk->bitsPerSample == 64)
+            _format = BufferFormat::MonoDouble;
+        else if(formatChunk->numChannels == 2 && formatChunk->bitsPerSample == 64)
+            _format = BufferFormat::StereoDouble;
+        else {
+            Error() << "Audio::WavImporter::openData(): IEEE with unsupported channel count"
+                    << formatChunk->numChannels << "with" << formatChunk->bitsPerSample
+                    << "bits per sample";
+            return;
+
+  if(formatChunk->blockAlign != formatChunk->numChannels * formatChunk->bitsPerSample / 8 ||
+       formatChunk->byteRate != formatChunk->sampleRate * formatChunk->blockAlign) {
+        Error() << "Audio::WavImporter::openData(): the file is corrupted";
+        return;
+    }
